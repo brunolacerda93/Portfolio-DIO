7ecab10d66cf8c5544d24b88c7aa5db8ff492d4e
Bruno Lacerda <bruno.lacerda9393@gmail.com>
2022-03-09 18:10:43 -0300

    Upload do Desafio de Exploração de Projetos com Java da DIO

---
+++ Projetos/Padrões_Projeto/src/application/Test.java
@@ -0,0 +1,57 @@
+package application;
+
+import facade.Facade;
+import singleton.SingletonEager;
+import singleton.SingletonLazy;
+import singleton.SingletonLazyHolder;
+import strategy.Comportamento;
+import strategy.ComportamentoAgressivo;
+import strategy.ComportamentoDefensivo;
+import strategy.ComportamentoNormal;
+import strategy.Robo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		// Teste relacionados ao Desgin Pattern Singleton
+		
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		
+		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		
+		// Strategy
+		
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		robo.setStrategy(normal);
+		robo.mover();
+		robo.mover();
+		robo.setStrategy(defensivo);
+		robo.mover();
+		robo.setStrategy(agressivo);
+		robo.mover();
+		robo.mover();
+		robo.mover();
+		
+		// Facade
+		
+		Facade facade = new Facade();
+		facade.migrarCliente("Björn", "09369123");
+	}
+}
